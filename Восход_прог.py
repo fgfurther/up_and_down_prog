e75f86de1878a30a34be91393d6434313aed7a9b
fgfuther <joventroj@gmail.com>
2023-11-14 19:56:40 +0700

    Добавление меню для визуализации кода

--- Восход_прог.py
+++ Восход_прог.py
@@ -102,10 +102,42 @@ def print_matrix(matrix):
     for row in matrix:
         print(" ".join(map(str, row)))
 
+def menu():
+    print("\nМеню:")
+    print("1. Ввести матрицу")
+    print("2. Сгенерировать случайную матрицу")
+    print("3. Выполнить алгоритм")
+    print("4. Вывести результат (матрицу)")
+    print("5. Завершить работу")
+
+def main():
+    while True:
+        menu()
+
+        choice = input("Выберите пункт меню: ")
+
+        if choice == '1':
+            enter_matrix(matrix)
+            result = None  # Сбрасываем результат при вводе новой матриц
+
+        elif choice == '2':
+            enter_rand_matrix(matrix)
+            result = None  # Сбрасываем результат при вводе новой матрицы
+
+        elif choice == '3':
+            make_matr(matrix)
+
+        elif choice == '4':
+            res(matrix, result)
+
+        elif choice == '5':
+            print("Завершение работы программы.")
+            break
+        else:
+            print("Некорректный выбор. Пожалуйста, выберите существующий пункт меню.")
+            input("Нажмите Enter для продолжения...")
+
+
 if __name__ == "__main__":
-    enter_matrix(matrix)
-    matrix=enter_rand_matrix(matrix)
-    result=make_matr(matrix)
-    res(matrix, result)
-    print_matrix(matrix)
+    main()
 
