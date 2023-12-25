54e499c8aec8aaa2d152cdcfe0aa4fd30bdf73d7
fgfuther <joventroj@gmail.com>
2023-12-25 20:58:14 +0700

    эффективное программирование примеры

--- эффективное-программирование.py
+++ эффективное-программирование.py
@@ -1,91 +1,84 @@
-import random
-from collections import Counter
-import os, psutil
+import tracemalloc
+from functools import reduce
+import numpy as np
+def sum_even_numbers(n):
+    return sum(range(0, n, 2))
 
-def input_matrix():
-    while True:
-        try:
-            rows, cols = map(int, input("Введите количество строк и столбцов матрицы (через пробел): ").split())
-            return [list(map(int, input(f"Введите элементы строки {i} через пробел: ").split()[:cols])) for i in range(rows)]
+def factorial(n):
+    return reduce(lambda x, y: x * y, range(1, n + 1), 1)
 
-        except ValueError:
-            print("Ошибка ввода. Пожалуйста, введите целочисленные значения.")
-
+def unique_elements(lst):
+    return list(set(lst))
 
 
+def matrix_multiply(matrix_a, matrix_b):
+    return [[sum(a * b for a, b in zip(row, col)) for col in zip(*matrix_b)] for row in matrix_a]
 
-def generate_random_matrix():
-    try:
-        rows, cols = map(int, input("Введите количество строк и столбцов матрицы (через пробел): ").split())
-        a, b = map(int, input("Введите нижнюю и верхнюю границы элементов (через пробел): ").split())
-        return [[random.randint(a, b) for _ in range(cols)] for _ in range(rows)]
-    except ValueError:
-        print("Ошибка ввода. Пожалуйста, введите целочисленные значения.")
+def sum_even_numbers1(n):
+    even_numbers = []
+    for i in range(n):
+        if i % 2 == 0:
+            even_numbers.append(i)
+    return sum(even_numbers)
 
-
-
-def process_matrix(matrix):
-    flat_matrix = [element for row in matrix for element in row]
-    element_counts = Counter(flat_matrix)
-    return [[element_counts[element] for element in row] for row in matrix]
-
-
-def make_matrix(matrix):
-    return process_matrix(matrix) if matrix else None
-
-
-def display_result(matrix, result):
-    if result:
-        print_matrix(matrix)
-        print("Результат:")
-        print_matrix(result)
+# Пример 2: Факториал числа
+def factorial1(n):
+    if n == 0:
+        return 1
     else:
-        print("Сначала выполните алгоритм.")
+        return n * factorial(n - 1)
+
+# Пример 3: Поиск уникальных элементов в списке
+def unique_elements1(lst):
+    result = []
+    for elem in lst:
+        if elem not in result:
+            result.append(elem)
+    return result
 
 
-def print_matrix(matrix):
-    for row in matrix:
-        print(" ".join(map(str, row)))
+
+# Пример 5: Умножение матриц
+def matrix_multiply1(matrix_a, matrix_b):
+    result = []
+    for i in range(len(matrix_a)):
+        row = []
+        for j in range(len(matrix_b[0])):
+            element = 0
+            for k in range(len(matrix_b)):
+                element += matrix_a[i][k] * matrix_b[k][j]
+            row.append(element)
+        result.append(row)
+    return result
 
 
-def main():
-    matrix = None
-    while True:
-        print("\nМеню:")
-        print("1. Ввести матрицу")
-        print("2. Сгенерировать случайную матрицу")
-        print("3. Выполнить алгоритм")
-        print("4. Вывести результат (матрицу)")
-        print("5. Завершить работу")
-
-        choice = input("Выберите пункт меню: ")
-
-        if choice == '1':
-            matrix = input_matrix()
-
-        elif choice == '2':
-            matrix = generate_random_matrix()
-            process = psutil.Process()
-            print(process.memory_info().rss)
-
-        elif choice == '3':
-            result = make_matrix(matrix)
-            if result:
-                print("Алгоритм выполнен.")
-            else:
-                print("Сначала введите или сгенерируйте матрицу.")
-
-        elif choice == '4':
-            display_result(matrix, make_matrix(matrix))
-
-        elif choice == '5':
-            print("Завершение работы программы.")
-            break
-
-        else:
-            print("Некорректный выбор. Пожалуйста, выберите существующий пункт меню.")
-            input("Нажмите Enter для продолжения...")
-
+def measure_memory_usage(func, *args, **kwargs):
+    tracemalloc.start()
+    result = func(*args, **kwargs)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    print(f"Использование памяти: {current / 10**6} MB (пиковое: {peak / 10**6} MB)")
+    return result
 
 if __name__ == "__main__":
-    main()
+    print("1. Сумма четных чисел")
+    result_1 = measure_memory_usage(sum_even_numbers, 1000000)
+    result_11 = measure_memory_usage(sum_even_numbers1, 1000000)
+
+    print("\n2. Вычисление факториала")
+    result_2 = measure_memory_usage(factorial, 100000)
+    result_22 = measure_memory_usage(factorial1, 100000)
+
+    print("\n3. Уникальные элементы списка")
+    result_3 = measure_memory_usage(unique_elements, range(1, 10001))
+    result_33 = measure_memory_usage(unique_elements1, range(1, 10001))
+
+
+    print("\n5. Умножение матриц")
+
+    matrix_a = np.random.randint(1000, size=(50, 50))
+
+    matrix_b = np.random.randint(1000, size=(50, 50))
+
+    result_5 = measure_memory_usage(matrix_multiply, matrix_a, matrix_b)
+    result_55 = measure_memory_usage(matrix_multiply1, matrix_a, matrix_b)
